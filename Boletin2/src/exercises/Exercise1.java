30670296cc485ae6f003a4c0f5ffeedc06c47c6e
misorna <misorna@106-31.info2>
2022-10-11 12:25:45 +0200

    Commit 1

---
+++ Boletin2/src/exercises/Exercise1.java
@@ -0,0 +1,18 @@
+package exercises;
+
+/***
+ * 
+ * Pedir una nota entera de 0 a 10 y mostrarla de la siguiente forma:
+ * insuficiente (de 0 a 4), suficiente (5), bien (6), notable (7 y 8) y
+ * sobresaliente (9 y 10).
+ * 
+ * @author misorna
+ *
+ */
+public class Exercise1 {
+
+	public static void main(String[] args) {
+int nota;
+	}
+
+}
