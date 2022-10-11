0db2a026027814d6bc2f602d2e843d5f24e64963
misorna <misorna@106-31.info2>
2022-10-11 12:41:11 +0200

    Commit 2

--- Boletin2/src/exercises/Exercise1.java
+++ Boletin2/src/exercises/Exercise1.java
@@ -1,5 +1,7 @@
 package exercises;
 
+import java.util.Scanner;
+
 /***
  * 
  * Pedir una nota entera de 0 a 10 y mostrarla de la siguiente forma:
@@ -12,7 +14,36 @@ package exercises;
 public class Exercise1 {
 
 	public static void main(String[] args) {
-int nota;
-	}
+		// Bloque de variables
+		int nota;
+		Scanner sc = new Scanner(System.in);
 
+		// Pedimos al usuario
+		System.out.print("Introduzca la nota: ");
+		// Leemos de teclado
+		nota = sc.nextInt();
+
+		// mostramos resultado segun la nota introducida
+		switch (nota) {
+		case 0, 1, 2, 3, 4:
+			System.out.println("Insuficiente");
+			break;
+		case 5:
+			System.out.println("Suficiente");
+			break;
+		case 6:
+			System.out.println("Bien");
+			break;
+		case 7, 8:
+			System.out.println("Notable");
+			break;
+		case 9, 10:
+			System.out.println("Sobresaliente");
+			break;
+		default:
+			System.out.println("Este valor no es valido");
+			break;
+		}
+
+	}
 }
